c21ad47dc70d7859f8666e8738fb2c6193950e77
roxyroxy <rroxanaioana@gmail.com>
2014-04-04 11:39:49 +0300

    package changed

--- src/services/VMServiceImpl.java
+++ src/services/VMServiceImpl.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package implementations;
+package services;
 import client.OpenNebulaClient;
 import java.util.List;
 
